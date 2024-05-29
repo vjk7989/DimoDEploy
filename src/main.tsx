3539003c31b2b9ae1262a1f6c3237c3f59027bc2
vjk7989 <92521801+vjk7989@users.noreply.github.com>
2024-05-29 23:53:59 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -11,9 +11,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryProvider>
-        <AuthProvider>
+        
           <App />
-        </AuthProvider>
+        
       </QueryProvider>
     </BrowserRouter>
   </React.StrictMode>
